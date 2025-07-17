a2d444a4920000f7911a34f4f765f63c9ccc4d99
Rojenn Clyde Patigdas <patigdasr@gmail.com>
2025-07-17 21:24:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 import express from "express"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}))
